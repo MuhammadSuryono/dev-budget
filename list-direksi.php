5f5dd51cecd4c85f794cec14959a53bbaf85957b
Muhammad Suryono <msuryono0@gmail.com>
2021-12-09 10:01:14 +0700

    Fix message

--- list-direksi.php
+++ list-direksi.php
@@ -137,7 +137,8 @@ $subTabUmum = ['Rutin', 'Non-Rutin']
                 <?php
                 
                 ?>
-                <ul class="nav navbar-nav navbar-right">
+               <ul class="nav navbar-nav navbar-right">
+                        <li><a href="notif-page.php"><i class="fa fa-envelope"></i></a></li>
                     <li><a href="ubahpassword.php"><span class="glyphicon glyphicon-user"></span><?php echo $_SESSION['nama_user']; ?> (<?php echo $_SESSION['divisi']; ?>)</a></li>
                     <li><a href="logout.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
                 </ul>
--- list.php
+++ list.php
@@ -95,8 +95,10 @@ if (!isset($_SESSION['nama_user'])) {
         </ul>
 
         
-        <ul class="nav navbar-nav navbar-right">
-          <ul class="nav navbar-nav navbar-right">
+       <ul class="nav navbar-nav navbar-right">
+                        <li><a href="notif-page.php"><i class="fa fa-envelope"></i></a></li>
+         <ul class="nav navbar-nav navbar-right">
+                        <li><a href="notif-page.php"><i class="fa fa-envelope"></i></a></li>
             <li><a href="ubahpassword.php"><span class="glyphicon glyphicon-user"></span><?php echo $_SESSION['nama_user']; ?> (<?php echo $_SESSION['divisi']; ?>)</a></li>
             <li><a href="logout.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
           </ul>
