d538b0442e93be16526b1c64e73f8e25ad0d863e
Muhammad Suryono <msuryono0@gmail.com>
2021-12-31 15:46:33 +0700

    Update data antrian

--- antrian-transfer-proses.php
+++ antrian-transfer-proses.php
@@ -5,6 +5,12 @@ require "application/config/database.php";
 $con = new Database();
 $koneksi = $con->connect();
 
+$con->set_name_db(DB_TRANSFER);
+$koneksiTransfer = $con->connect();
+
+$time = date("Y-m-d H:i:s");
+$user = $_SESSION['nama_user'];
+
 $id = $_POST['id'];
 $button = $_POST['button'];
 $from = $_POST['from'];
@@ -17,11 +23,11 @@ $getData = mysqli_query($koneksiTransfer, "SELECT * FROM data_transfer WHERE tra
 $data = mysqli_fetch_assoc($getData);
 
 if ($button == 'edit') {
-    $update = mysqli_query($koneksiTransfer, "UPDATE data_transfer SET jadwal_transfer = '$jadwal_transfer', ket_tambahan='$ket_tambahan' WHERE transfer_id = '$id'");
+    $update = mysqli_query($koneksiTransfer, "UPDATE data_transfer SET jadwal_transfer = '$jadwal_transfer', updated_at='$time', updated_by='$user', ket_tambahan='$ket_tambahan' WHERE transfer_id = '$id'");
 }
 
 if ($button == 'cancel') {
-    $update = mysqli_query($koneksiTransfer, "UPDATE data_transfer SET ket_transfer = 'Cancel', hasil_transfer = '4', ket_tambahan='$ket_tambahan', jadwal_transfer = null WHERE transfer_id = '$id'");
+    $update = mysqli_query($koneksiTransfer, "UPDATE data_transfer SET ket_transfer = 'Cancel', updated_at='$time', updated_by='$user', hasil_transfer = '4', ket_tambahan='$ket_tambahan', jadwal_transfer = null WHERE transfer_id = '$id'");
 }
 
 if ($button == 'antri') {
@@ -34,7 +40,7 @@ if ($button == 'antri') {
 
     $formatId = $date . sprintf('%04d', $count + 1);
 
-    $update = mysqli_query($koneksiTransfer, "UPDATE data_transfer SET ket_transfer = 'Antri', transfer_req_id = '$formatId', jadwal_transfer = '$jadwal_transfer', hasil_transfer = '1' WHERE transfer_id = '$id'");
+    $update = mysqli_query($koneksiTransfer, "UPDATE data_transfer SET ket_transfer = 'Antri', updated_at='$time', updated_by='$user', transfer_req_id = '$formatId', jadwal_transfer = '$jadwal_transfer', hasil_transfer = '1' WHERE transfer_id = '$id'");
 }
 if ($update) {
     echo "<script language='javascript'>";
